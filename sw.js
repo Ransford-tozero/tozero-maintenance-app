b33ba191db02b02b499988a92f1733330f4af86d
Ransford <ransford@tozero.solutions>
2026-09-24 10:52:51 +0200

    Initial PWA deployment with QR scanner and history timeline

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'maint-pwa-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/html5-qrcode'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
